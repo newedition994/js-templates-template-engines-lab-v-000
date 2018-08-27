3023442cc434b7bc8a69d098307f733ac735b6cd
Breyanna Vass <breyanna.bvass@gmail.com>
2018-08-27 20:20:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,36 +1,24 @@
-function postComment() {
-    var commenter = document.getElementById("commenterName").value;
-    var comment = document.getElementById("commentText").value;
-
-    var commentTemplate = document.getElementById("comment-template").innerHTML;
-    var templateFn = _.template(commentTemplate);
-
-    var commentsDiv = document.getElementById("comments");
-    var templateHTML = templateFn({ 'cooment': comment, 'commenter': commenter });
-    commentsDiv.innerHTMl += templateHTML;
-}
-
 function createPost() {
-    var author = document.getElementById("author").value;
-    var body = document.getElementById("body").value;
-    var title = document.getElementById("title").value;
-
-    var pageTemplate = document.getElementById("page-template").innerHTML;
-    var pageTemplateFn = _.template(pageTemplate);
-
-    var postTemplate = document.getElementById("post-template".innerHTML);
-    var postTemplateFn = _.template(postTemplate);
-
-    var commentsTemplate = document.getElementById("comments-template").innerHTML;
-    var commentsTemplateFn = _.template(commentsTemplate);
-
-    var pageDiv = document.getElementById("page");
-    pageDiv.innerHTML += pageTemplateFn();
-
-    var postTemplateHTML = postTemplateFn({ 'title': title, 'body': body, 'author': author });
-    var commentsTemplateHTML = commentsTemplateFn();
-    var postDiv = document.getElementById("post");
-
-    postDiv.innerHTML = postTemplateHTML;
-    postDiv.getElementsByTagName("article")[0].innerHTML = commentsTemplateHTML
+  
+  var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  var postTemplate = _.template(document.getElementById("post-template").innerHTML);
+  var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
+   
+  var postTitle = document.getElementById("postTitle").value;
+  var postAuthor = document.getElementById("postAuthor").value;
+  var post = document.getElementById("postBody").value;
+   document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+   var blogSection = postTemplate({ 'title': postTitle, 'body': post, 'poster': postAuthor });
+  
+  var commentsSection = commentsTemplate();
+  var postElement = document.getElementById("post");
+   postElement.innerHTML = blogSection;
+  postElement.getElementsByTagName("footer")[0].innerHTML = commentsSection;
+}
+ function postComment() {
+  var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+   var commentText = document.getElementById("commentText").value;
+  var commenterName = document.getElementById("commenter").value;
+   var commentsSection = document.getElementById("comments");
+  commentsSection.innerHTML += commentTemplate({ 'commenter': commenterName, 'comment': commentText });
 }
